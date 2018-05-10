cc88f00576a84c052f91225d57d24a1af54883d6
Nguyen Thi Hao <haonguyen4993@gmail.com>
2018-05-10 16:09:11 +0700

    Chapter 7 - Sign up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,18 @@ module ApplicationHelper
     base_title = I18n.t "layouts.application.base_title"
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
+
+  # Returns alert msg css class
+  def alert_msg alert_type
+    case alert_type
+    when "success"
+      "alert alert-success"
+    when "danger"
+      "alert alert-danger"
+    when "error"
+      "alert alert-error"
+    else
+      "alert"
+    end
+  end
 end
